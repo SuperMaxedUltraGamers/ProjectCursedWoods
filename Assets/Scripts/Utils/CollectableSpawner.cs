99d102516249b1a58b8d09df5b81c2230f000c55
Eetu Pohja <eetu.pohja@tuni.fi>
2021-03-29 02:59:38 +0300

    Collectable spawner

---
+++ Assets/Scripts/Utils/CollectableSpawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CursedWoods.Utils
+{
+    public class CollectableSpawner : MonoBehaviour
+    {
+        private void Start()
+        {
+            IPoolObject maxHealthIncrease = GameMan.Instance.ObjPoolMan.GetObjectFromPool(ObjectPoolType.MaxHealthIncrease);
+            maxHealthIncrease.Activate(transform.position, transform.rotation);
+        }
+    }
+}
