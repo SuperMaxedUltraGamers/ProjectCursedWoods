25a07178d51801206d4cfbd3105e6b6256349f2f
Eetu Pohja <eetu.pohja@tuni.fi>
2021-03-31 18:06:50 +0300

    Nevermind ville, got it fixed

--- Assets/Scripts/ParticleEffects/IceRaycastParticleMover.cs
+++ Assets/Scripts/ParticleEffects/IceRaycastParticleMover.cs
@@ -20,22 +20,21 @@ namespace CursedWoods
 
         private void Update()
         {
+            if (elapsedTime == 0f)
+            {
+                mainParticles.Play(withChildren: true);
+            }
+
             if (elapsedTime < lerpTime)
             {
                 elapsedTime += Time.deltaTime;
                 transform.position = Vector3.Lerp(startPos, endPos, elapsedTime / lerpTime);
-            }
-            else
-            {
-                //mainParticles.Stop(withChildren: true, ParticleSystemStopBehavior.StopEmittingAndClear);
             }
         }
 
         public void Initialize(Vector3 start, Vector3 end)
         {
             mainParticles.Stop(withChildren: true, ParticleSystemStopBehavior.StopEmittingAndClear);
-            mainParticles.Play(withChildren: true);
-
             startPos = start;
             endPos = end;
             elapsedTime = 0f;
